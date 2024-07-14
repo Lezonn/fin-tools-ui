d6472e8d011f3ae361cb26e587209c580e8256e3
Leonard Zonaphan <leonard.zonaphan@gdn-commerce.com>
2024-07-14 12:54:34 +0700

    implement vuetify and sass

--- src/main.js
+++ src/main.js
@@ -2,15 +2,26 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import vue3GoogleLogin from 'vue3-google-login'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives
+})
 
 app.use(createPinia())
 app.use(router)
 app.use(vue3GoogleLogin, {
   clientId: '640757226227-3o3ein8lr4l0v5tfu5t7hp8qfojnc9mr.apps.googleusercontent.com'
 })
+app.use(vuetify)
 
 app.mount('#app')
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,7 +1,10 @@
 <script setup>
+import { useRouter } from 'vue-router'
 import { googleAuthCodeLogin } from 'vue3-google-login'
 import { api } from '@/config'
 import { safeGet } from '@/utils/http'
+
+const router = useRouter()
 
 const login = async () => {
   const authResponse = await googleAuthCodeLogin()
@@ -17,12 +20,29 @@ const login = async () => {
     return
   }
 
-  console.log(callbackResponse)
+  if (callbackResponse.data) {
+    router.push({ name: 'home' })
+  }
 }
 </script>
 
 <template>
-  <div>
-    <button @click="login">Login Using Google</button>
-  </div>
+  <v-container>
+    <div class="login-form">
+      <h2 class="text-center">Login</h2>
+      <div class="text-center my-4">
+        <button @click="login">Login Using Google</button>
+      </div>
+    </div>
+  </v-container>
 </template>
+
+<style lang="scss" scoped>
+.login-form {
+  border: 1px solid black;
+  box-shadow: 1px 1px rgb(85, 85, 85);
+  height: 150px;
+  width: 300px;
+  margin: 10vh auto;
+}
+</style>
