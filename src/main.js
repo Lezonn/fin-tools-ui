0c1ae5c3a850f24e41c1059597de00fb75a5c843
Leonard Zonaphan <leonard.zonaphan@gdn-commerce.com>
2024-07-22 14:23:17 +0700

    set axios default withCredentials true

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import vue3GoogleLogin from 'vue3-google-login'
+import axios from 'axios'
 
 // Vuetify
 import 'vuetify/styles'
@@ -12,16 +13,20 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
+const pinia = createPinia()
+const googleClientId = {
+  clientId: '640757226227-3o3ein8lr4l0v5tfu5t7hp8qfojnc9mr.apps.googleusercontent.com'
+}
 const vuetify = createVuetify({
   components,
   directives
 })
 
-app.use(createPinia())
+axios.defaults.withCredentials = true
+
+app.use(pinia)
 app.use(router)
-app.use(vue3GoogleLogin, {
-  clientId: '640757226227-3o3ein8lr4l0v5tfu5t7hp8qfojnc9mr.apps.googleusercontent.com'
-})
+app.use(vue3GoogleLogin, googleClientId)
 app.use(vuetify)
 
 app.mount('#app')
