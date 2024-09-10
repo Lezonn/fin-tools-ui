7422aa18823af2dee539539ecb3f3c2be7860ab2
Leonard Zonaphan <leonard.zonaphan@gdn-commerce.com>
2024-09-10 16:04:14 +0700

    implement add expense form

--- src/main.js
+++ src/main.js
@@ -6,6 +6,8 @@ import vue3GoogleLogin from 'vue3-google-login'
 import 'vuetify/styles'
 import '@mdi/font/css/materialdesignicons.css'
 import { createVuetify } from 'vuetify'
+import { VDateInput } from 'vuetify/labs/VDateInput'
+import { VNumberInput } from 'vuetify/labs/VNumberInput'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
@@ -14,11 +16,17 @@ import router from './router'
 
 const app = createApp(App)
 const pinia = createPinia()
+
 const googleClientId = {
   clientId: '640757226227-3o3ein8lr4l0v5tfu5t7hp8qfojnc9mr.apps.googleusercontent.com'
 }
+
 const vuetify = createVuetify({
-  components,
+  components: {
+    ...components,
+    VDateInput,
+    VNumberInput
+  },
   directives
 })
 
